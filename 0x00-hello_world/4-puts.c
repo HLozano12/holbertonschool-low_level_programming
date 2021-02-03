a637c4987b2605a40ecb7f0c620526565537d86b
HLozano12 <HLR8503@gmail.com>
2021-02-03 23:17:10 +0000

    Put it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
+/*
+ * main - My function
+ */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle"\n)
+	puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
+
 }
