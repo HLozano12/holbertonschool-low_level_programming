6b04c6109c13429a898ce6c33897baf350b7b569
HLozano12 <HLR8503@gmail.com>
2021-02-03 23:40:30 +0000

    PrintF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The Function
+ *
+ * Description: Should print out string
+ *
+ * Return: zero
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
