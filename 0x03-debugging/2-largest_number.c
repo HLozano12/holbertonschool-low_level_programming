ce2c885428a7588d52126cc85462a14a397a5544
HLozano12 <HLR8503@gmail.com>
2021-02-09 17:33:10 +0000

    >= update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,10 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+/* second b changed to a */
 	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
+
+/* second a was changed to b */
 	else if (b >= a && b >= c)
 	{
 		largest = b;
