dfee275fcb9db36bfb7230bb5f47248385c032ee
HLozano12 <HLR8503@gmail.com>
2021-02-11 22:02:02 +0000

    Product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Function
+ * Description: Multiply two numbers
+ * Return: Product of two intergers
+ * @a: int one
+ * @b: int two
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
