2b5be648fc3e9f709b7087b411df61ea6c7435e9
HLozano12 <HLR8503@gmail.com>
2021-02-24 04:47:33 +0000

    Dylan

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - Prototype function
+ * Description: set value of pointer to char
+ * @s: double pointer
+ * @to: pointer
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
