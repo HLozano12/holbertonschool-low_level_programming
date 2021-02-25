fa49ad9c2a3fae677e7197047cdb5b297334536e
HLozano12 <HLR8503@gmail.com>
2021-02-25 18:46:55 +0000

    Identical

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * wildcmp - function prototype
+ * Description: Compare two strings
+ * @s1: first string
+ * @s2: Second string
+ * Return: 1 if identical, else zero
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == *s2)
+		return (1);
+	else
+		return (0);
+}
