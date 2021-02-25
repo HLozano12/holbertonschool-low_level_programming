7cab2001702868cf27b107629400f219bb0b3daa
HLozano12 <HLR8503@gmail.com>
2021-02-25 04:35:38 +0000

    Rooted

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - Prototype function
+ * Description: return square root of a number
+ * @n:number to be square rooted
+ * Return: The natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	return (_find_sqrt(n, 0));
+}
+
+/**
+ * _find_sqrt - Made function to locate square root
+ * Description: find out what the square root number is
+ * @h: Interger provided
+ * @l: attempt of sqrt
+ * Return: the square root
+ */
+int _find_sqrt(int h, int l)
+{
+	if (l * l == h)
+		return (l);
+
+	if (l * l > h)
+		return (- 1);
+
+	else
+		return (_find_sqrt(h, l + 1));
+}
