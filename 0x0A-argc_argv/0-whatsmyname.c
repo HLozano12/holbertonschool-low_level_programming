15d0355ba0b4191ce6f5b5e6b73d584e069a97e1
HLozano12 <HLR8503@gmail.com>
2021-02-28 20:09:27 +0000

    MyNewName

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - main function
+ * Description: Print programs name
+ * @argc: Count of the arument
+ * @argv: Arguement value
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
