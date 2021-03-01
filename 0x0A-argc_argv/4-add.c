7dd531afddb3b25df5eea8c65e749a98e393e8fd
HLozano12 <HLR8503@gmail.com>
2021-03-01 05:20:48 +0000

    Positive add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,47 +6,39 @@
  * @argv: Arguement value
  * Return: zero if no # passed to the program, Error and 1 if not digit
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int h;
-	int sum = 0;
+	int l = 0;
 
-/* starting at array 1 and incriment */
-	for (h = 1; h < argc; h++)
-	{
-
-/* using previous proj function to check if varible is digit */
-		if (!_isdigit(*argv[h]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		sum += atoi(argv[h]);
-	}
-
-/* if the argument count is 1, no number passed to prog, print 0 */
-	if (argc == 1)
+/* If # is not passed to the program print 0 followed by new line */
+	if (argc <= 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	printf("%d \n", sum);
+/* Moving through the argument count not including the 1st count */
+	for (h = 1; h < argc; h++)
+	{
 
-	return (0);
-}
+/* if a number has symb that are not digits print error\n and 1 */
+		if ((*argv[h] < '0' ||
+*argv[h] > '9') && *argv[h] != '-')
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-/**
- * _isdigit - Our prototype
- * @c: int varible
- * Return: 1 if digit 0 other wise
- */
-int _isdigit(int c)
-{
-	/* if interger is greater than zero & less than 9 print 1 */
-	if (c >= 48 && c <= 57)
-		return (1);
-	/* otherwise print zero  */
+
+/* If anything else, provide the sum of positive numbers and return 0 */
+		else
+		{
+			l += atoi(argv[h]);
+		}
+
+	}
+
+	printf("%d\n", l);
 	return (0);
 }
