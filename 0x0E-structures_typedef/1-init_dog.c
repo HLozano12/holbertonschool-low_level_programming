e759789e6a6eca44b24a9dc7eef92d9aa4b08277
HLozano12 <HLR8503@gmail.com>
2021-03-08 20:43:23 +0000

    Initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * int_dog - Prototype function
+ * Decription: Initialize a varible of type struct dog
+ * @d: Struct
+ * @name: name of dog
+ * @age: Age of dog
+ * @owner: owner of dog
+ * Return: Void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
