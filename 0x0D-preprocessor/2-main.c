979da670226ebf60e3f9e74aa6ee2c5e9402a443
HLozano12 <HLR8503@gmail.com>
2021-03-08 18:45:53 +0000

    Process File

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - Main Function
+ * Description: Print name of file compiled
+ * Return: Void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
