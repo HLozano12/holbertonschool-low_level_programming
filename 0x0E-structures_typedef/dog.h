db822b58818520121dc53309e3dac61f67337659
HLozano12 <HLR8503@gmail.com>
2021-03-08 19:41:00 +0000

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
-#ifndef DOG_H
+B#ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+
+ /**
+  * struct dog - struct info
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: Owner of dog
+  */
 
 struct dog
 {
