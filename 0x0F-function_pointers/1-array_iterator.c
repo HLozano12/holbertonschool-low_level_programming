ca2a3209c0824460ba520a4f2365ab5af6ddd9e4
HLozano12 <HLR8503@gmail.com>
2021-03-11 03:01:39 +0000

    Element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Function Prototype
+ * Description: Execute a function given as a parameter each element of array
+ * @array: Our array
+ * @size: our size of array
+ * @action: Our function pointer
+ * Return: voide accoriding to prototype
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t h;
+
+	if (action != NULL && array != NULL)
+	{
+		for (h =0; h < size; h++)
+			action(array[h]);
+	}
+}
