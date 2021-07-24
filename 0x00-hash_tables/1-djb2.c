25b06bb723147e2ac982be8252786dc4f7bda6a0
HLozano12 <HLR8503@gmail.com>
2021-07-24 15:54:00 +0000

    djb2

---
+++ 0x00-hash_tables/1-djb2.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * write a hash func implementing the djb2 algorithm
+ * function is from task given
+ * using hash_djb2.c from papamuziko GitHub
+ */
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
