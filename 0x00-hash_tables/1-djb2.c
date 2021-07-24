cbcce7992e784c34e9f41848264910cb8b18b80f
HLozano12 <HLR8503@gmail.com>
2021-07-24 19:09:46 +0000

    Borrow Djb2

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,12 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * write a hash func implementing the djb2 algorithm
- * function is from task given
- * using hash_djb2.c from papamuziko GitHub
- */
-
-/**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
  *
