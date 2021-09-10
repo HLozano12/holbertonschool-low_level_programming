6e1874a486977375bdcbb059507b610b654ef07d
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-10 09:35:56 -0500

    Not C# comments

--- 0x02-search_algorithms/search_algos.h
+++ 0x02-search_algorithms/search_algos.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Given Prototypes
+/* Given Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
