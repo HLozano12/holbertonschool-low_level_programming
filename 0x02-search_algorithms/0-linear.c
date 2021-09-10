3f109c5e03bae3834f3c4be88d6e7ec2ca460a55
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-10 09:32:08 -0500

    Load

---
+++ 0x02-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Sequentially checks each element of a list until match found
+ * @array: The array we are searching in
+ * @size: the size of the array
+ * @value: the value to search for
+ * 
+ * Return: The index of value in the array or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t shirt;
+
+	for (shirt = 0; shirt < size && array != NULL; shirt++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)shirt, array[shirt]);
+		if (array[shirt] == value)
+			return ((int)shirt);
+	}
+	return (-1);
+}
---
+++ 0x02-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include <search_algos.h>
+
+/**
+ * binary_search - Search for val in a sorted array of ints using binary search
+ * @array: a pointer to the first element of the array to search
+ * @size: the number of elements in array
+ * @value: value to search for
+ * 
+ * Return: index where value is, or -1
+ */
+ 
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
